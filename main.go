795d00302e55cd0c0aff379484725ae6512f6d88
wbtiger <28288271@qq.com>
2026-05-17 14:14:32 +0800

    Revert "Add TUI foundation (Phase 1: Harness Foundation)"
    This reverts commit 0ae44e9a648960b5cc35c1e08d67c1e98133bcb0.

--- main.go
+++ main.go
@@ -1,39 +1,12 @@
 package main
 
 import (
-	"fmt"
 	"os"
 
-	tea "github.com/charmbracelet/bubbletea"
-
 	"github.com/gitlink-org/gitlink-cli/cmd"
-	"github.com/gitlink-org/gitlink-cli/tui"
 )
 
 func main() {
-	if len(os.Args) > 1 && os.Args[1] == "tui" {
-		// TUI mode
-		f, err := tea.LogToFile("gitlink-tui.log", "")
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "failed to init log: %v\n", err)
-			os.Exit(1)
-		}
-		defer f.Close()
-
-		app := tui.NewApp()
-		program := tea.NewProgram(
-			app,
-			tea.WithAltScreen(),
-			tea.WithMouseCellMotion(),
-		)
-		if _, err := program.Run(); err != nil {
-			fmt.Fprintf(os.Stderr, "error: %v\n", err)
-			os.Exit(1)
-		}
-		return
-	}
-
-	// CLI mode
 	if err := cmd.Execute(); err != nil {
 		os.Exit(1)
 	}
